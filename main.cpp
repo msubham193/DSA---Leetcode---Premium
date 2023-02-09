c06b35154959e3f084949d96f9f772717364f27a
Subham Mishra <>
2023-02-09 05:34:43 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+int main() {
+	// your code goes here
+
+	int test;
+
+	cin >> test;
+
+	while (test--) {
+		solve();
+	}
+
+	return 0;
+}
+
+void solve() {
+
+
+
+
+    for(int i=-1;i<=+1;i++){
+
+          for(int j=-1;j<=+1;j++){
+
+
+               cout<<
+
+
+              
+          }
+
+        
+    }
+    
+
+
+
+
+    
+    
+}
